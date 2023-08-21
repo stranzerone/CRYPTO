937e2306fb4dba9186fcc5ef879cfc8912a08f72
stranzerone <113999437+stranzerone@users.noreply.github.com>
2023-08-21 13:02:09 +0530

    nn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ mongoose.connect(database).then(
 
 
 app.use(bodyParser.json());
-app.use( "*", cors({origin: "http://localhost:3000", credentials: true }));
+app.use( "*", cors({origin: "http://localhost:3000",allowedHeaders:true, credentials: true }));
 
 
 
